5cbb24b9bef56985a10a105732505c43a66473a7
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-04-20 20:12:04 +0530

    login & user models updated

--- EMS/login/models.py
+++ EMS/login/models.py
@@ -5,9 +5,9 @@ from django.db import models
 from django.db import models
 
 class user_login(models.Model):
-    user_name = models.CharField(max_length=30)
     user_roll_no = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    remember_me = models.BooleanField(default=False)
     
     def __str__(self):
         return self.user_roll_no
--- EMS/user/models.py
+++ EMS/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    uid = models.CharField(primary_key=True, max_length=10)  # Assuming a maximum length of 10 characters for alphanumeric UID
+    user_name = models.CharField(max_length=30)
+    user_roll_no = models.CharField(max_length=30, unique=True)  # Assuming alphanumeric and unique roll numbers
+    user_room_no = models.CharField(max_length=10)  # Assuming alphanumeric room numbers
+    user_current_status = models.BooleanField(default=True)  # Assuming a boolean field for current status of student
+
+    def __str__(self):
+        return self.user_roll_no
+    
