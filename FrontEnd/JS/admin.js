982beca8da5acd598a7afe9c5864d4be8985b91a
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-04-21 02:08:17 +0530

    admin page layout done

--- FrontEnd/JS/admin.js
+++ FrontEnd/JS/admin.js
@@ -1,16 +1,16 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
+// let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
   menuBtnChange();
 });
 
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
-});
+// searchBtn.addEventListener("click", () => {
+//   sidebar.classList.toggle("open");
+//   menuBtnChange();
+// });
 
 function menuBtnChange() {
   if (sidebar.classList.contains("open")) {
