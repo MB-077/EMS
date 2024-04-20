a30a2a4e1852e9f20845d6e6401c68ad3460e449
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-04-20 19:50:56 +0530

    added login model

--- EMS/login/models.py
+++ EMS/login/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+class user_login(models.Model):
+    user_name = models.CharField(max_length=30)
+    user_roll_no = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.user_roll_no
