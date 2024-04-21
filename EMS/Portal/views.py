5ddd550129862e530611ce5a9f94dac36ab18cc7
Ritwik Gupta <ritwikg.0501@gmail.com>
2024-04-21 07:44:18 +0530

    Re-connected Everything

--- EMS/Portal/views.py
+++ EMS/Portal/views.py
@@ -21,16 +21,21 @@ def dashboard(request):
         print(f'\n{n}\n')
         for pk in n:
             data = {}
-            data["student_name"] = Student.objects.get(pk=pk).student_name
-            data["student_roll_no"] = Student.objects.get(pk=pk).student_roll_no
-            data["student_room_no"] = Student.objects.get(pk=pk).student_room_no
-            data["room_light_status"] =  Room.objects.get(pk=pk).room_light_status
-            data["student_hostel_out_time"] = Student.objects.get(pk=pk).student_hostel_out_time
-            data["student_hostel_status"] = Student.objects.get(pk=pk).student_hostel_status
+            data["student_name"] = Student.objects.get(pk=pk.student_roll_no).student_name
+            data["student_roll_no"] = Student.objects.get(pk=pk.student_roll_no).student_roll_no
+            data["student_room_no"] = Student.objects.get(pk=pk.student_roll_no).student_room_no
+            data["room_light_status"] =  change_e_status(Room.objects.get(pk=pk.student_roll_no).room_light_status)
+            data["student_hostel_out_time"] = Student.objects.get(pk=pk.student_roll_no).student_hostel_out_time
+            data["student_hostel_status"] = change_status(Student.objects.get(pk=pk.student_roll_no).student_hostel_status)
             datas.append(data)
         return render(request, "admin.html", {"datas" : datas, "username" : request.COOKIES.get("username")})
     else:
-        return render(request, "UserPage.html", {"username" : request.COOKIES.get("username")})
+        data = {}
+        data["student_name"] = Student.objects.get(pk=user.student_roll_no).student_name
+        data["student_room_no"] = Student.objects.get(pk=user.student_roll_no).student_room_no
+        data["student_hostel_status"] = change_status(Student.objects.get(pk=user.student_roll_no).student_hostel_status)
+
+        return render(request, "UserPage.html", data)
     
 
 def logout(request):
@@ -43,6 +48,7 @@ def auth_account(request):
     if request.method=="POST":
         entered_rollno = request.POST['name']
         entered_pass = request.POST['pass']
+        rem_me = request.POST.get('rem_me', False)
 
         try:
             user = User_Login.objects.get(student_roll_no=entered_rollno)
@@ -52,7 +58,8 @@ def auth_account(request):
         if user.password == entered_pass:
             response = HttpResponseRedirect(reverse("dashboard"))  # Set "username" cookie 
             response.set_cookie("username", entered_rollno)
-            if not user.remember_me:
+            if not rem_me:
+                user.remember_me = rem_me
                 request.session.set_expiry(0)
             else:
                 response.set_cookie("logged", "True")  # Set LOGGED as TRUE iff Remember_Me = True
@@ -60,3 +67,15 @@ def auth_account(request):
         else:
             # Redirect to Homepage.
             return HttpResponseRedirect(reverse("home"))
+
+def change_status(hostel_status):
+    if hostel_status == True:
+        return "Yes"
+    else :
+        return "No"
+
+def change_e_status(elec_status):
+    if elec_status == True:
+        return "On"
+    else :
+        return "Off"
